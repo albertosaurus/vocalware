34867a465852cb61a12d87ebc4d45f1815149001
Potapov Sergey <blake131313@gmail.com>
2013-10-25 13:20:39 +0300

    Add missing specs
    Task 12743

--- spec/lib/vocalware/client_spec.rb
+++ spec/lib/vocalware/client_spec.rb
@@ -1,21 +1,24 @@
 require 'spec_helper'
 describe Vocalware::Client do
-  let(:voice)       { Vocalware::Voice.find(:name => "Kate") }
-  let(:http_client) { double(:http_client) }
+  let(:voice)         { Vocalware::Voice.find(:name => "Kate") }
+  let(:http_client)   { double(:http_client)                   }
+  let(:secret_phrase) { 'Lucretia'                             }
+  let(:api_id)        { 2025                                   }
+  let(:account_id)    { 13                                     }
 
-  let(:client) do
-    client = described_class.new(
-      :account_id    => 13,
-      :api_id        => 2025,
-      :secret_phrase => "Lucretia",
+  let(:client_params) do
+    { :account_id    => account_id,
+      :api_id        => api_id,
+      :secret_phrase => secret_phrase,
       :voice         => voice,
       :ext           => 'swf',
       :host          => 'vocal.com',
       :protocol      => 'https',
       :port          => 4040,
-      :http_client   => http_client
-    )
+      :http_client   => http_client }
   end
+
+  let(:client) { described_class.new(client_params) }
 
   describe '.new' do
     it 'should initialize client attributes from hash' do
@@ -42,6 +45,30 @@ describe Vocalware::Client do
       client.host.should     == 'www.vocalware.com'
       client.path.should     == '/tts/gen.php'
       client.ext.should      == 'mp3'
+    end
+
+    context 'wrong parameter value' do
+      params = [:secret_phrase, :api_id, :account_id, :voice]
+
+      params.each do |param|
+        context "#{param} is missing" do
+          let(param) { nil }
+
+          it 'should raise Vocalware::Error' do
+            expect { described_class.new(client_params) }.
+              to raise_error(Vocalware::Error, "#{param} is missing")
+          end
+        end
+      end
+
+      context 'voice is not instance of Vocalware::Voice' do
+        let(:voice) { Object.new }
+
+        it 'should raise Vocalware::Error' do
+            expect { described_class.new(client_params) }.
+              to raise_error(Vocalware::Error, 'voice must be a Vocalware::Voice')
+        end
+      end
     end
   end
 
